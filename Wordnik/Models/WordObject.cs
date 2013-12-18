b4e120436d27afd9c9e84b012520795fbefaeb7c
Marcus Ball <marcus@tapshield.com>
2013-12-17 23:16:19 -0500

    added Scrabble property, and fixed Suggestions property in WordObject

--- Wordnik/Models/WordObject.cs
+++ Wordnik/Models/WordObject.cs
@@ -10,8 +10,9 @@ namespace Wordnik.Models {
         public int ID { get; set; }
         public string Word { get; set; }
         public string OriginalWord { get; set; }
-        public List<string> MyProperty { get; set; }
+        public List<string> Suggestions { get; set; }
         public string CanonicalForm { get; set; }
         public string Vulgar { get; set; }
+        public bool Scrabble { get; set; }
     }
 }
