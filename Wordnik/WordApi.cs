03caccdec025f76bb41b5f955505ef08680bfc26
Marcus Ball <marcus@tapshield.com>
2013-12-17 23:02:46 -0500

    Added documentation to WordApi GetExamples

--- Wordnik/WordApi.cs
+++ Wordnik/WordApi.cs
@@ -14,12 +14,36 @@ namespace Wordnik {
             : base(apiKey, apiServer) {
 
         }
+
+        /// <summary>
+        /// Returns examples for a word.
+        /// </summary>
+        /// <param name="word">The word to return examples for.</param>
+        /// <returns>ExampleSearchResults in the form of an async Task.</returns>
         public async Task<ExampleSearchResults> GetExamples(string word) {
             return await this.GetExamples(word, false, false, 0, 0);
         }
+
+        /// <summary>
+        /// Returns examples for a word.
+        /// </summary>
+        /// <param name="word">The word to return examples for.</param>
+        /// <param name="includeDuplicates">Show duplicate examples from different sources.</param>
+        /// <param name="useCanonical">If true will try to return the correct word root ('cats' -> 'cat'). If false returns exactly what was requested.</param>
+        /// <returns>ExampleSearchResults in the form of an async Task.</returns>
         public async Task<ExampleSearchResults> GetExamples(string word, bool includeDuplicates, bool useCanonical) {
             return await this.GetExamples(word,includeDuplicates,useCanonical,0,0);
         }
+
+        /// <summary>
+        /// Returns examples for a word.
+        /// </summary>
+        /// <param name="word">The word to return examples for.</param>
+        /// <param name="includeDuplicates">Show duplicate examples from different sources.</param>
+        /// <param name="useCanonical">If true will try to return the correct word root ('cats' -> 'cat'). If false returns exactly what was requested.</param>
+        /// <param name="skip">Results to skip</param>
+        /// <param name="limit">Maximum number of results to return.</param>
+        /// <returns>ExampleSearchResults in the form of an async Task.</returns>
         public async Task<ExampleSearchResults> GetExamples(string word, bool includeDuplicates, bool useCanonical, int skip, int limit) {
             if(word == null || String.IsNullOrWhiteSpace(word)){
                 throw new ArgumentException("Word argument cannot be null or empty.");
@@ -46,8 +70,8 @@ namespace Wordnik {
             try {
                 return JsonConvert.DeserializeObject<ExampleSearchResults>(response.ResponseText);
             }
-            catch (Exception e) {
-                Console.Error.WriteLine("Shit");
+            catch (JsonException e) {
+                Console.Error.WriteLine("Wordnik WordApi JsonException caught: [{0}] {1}", e.HResult, e.Message);
             }
             return null;
         }
